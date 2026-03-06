3c2d67fc3ae4582c00cf81cace9eab5d527c0594
fahmihwan <50146950+fahmihwan@users.noreply.github.com>
2026-03-06 21:50:42 +0700

    add isRequired rules

--- my-app/src/view/customer/pages/home/component/drawerAddCart/AddOnCompt.tsx
+++ my-app/src/view/customer/pages/home/component/drawerAddCart/AddOnCompt.tsx
@@ -8,29 +8,40 @@ interface AddOnComptProps {
     name: string; // name untuk group radio/checkbox
     type?: "radio" | "checkbox";
     checked: boolean,
-    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    onClick: React.MouseEventHandler<HTMLLIElement>;
     formatRupiah: (value: number) => string
 }
 
-const AddOnCompt = ({ opt, inputId, name, type, checked, onChange, formatRupiah }: AddOnComptProps) => {
+const AddOnCompt = ({ opt, inputId, name, type, checked, onClick, formatRupiah }: AddOnComptProps) => {
     return (
-        <li className="flex justify-between" ><span className="font-light text-lg">{opt.name}</span>
-            <div className="flex items-center mb-4">
-                <label
-                    htmlFor="default-radio-1"
-                    className="me-2 text-lg font-light text-gray-900 "
+        <li
+            className={`flex justify-between cursor-pointer rounded-md px-2 py-2 ${checked ? "bg-blue-50" : ""
+                }`}
+            onClick={onClick}
+        >
+            <span
+                className={`text-lg cursor-pointer ${checked ? "text-blue-600 font-medium" : "text-gray-900 font-light"
+                    }`}
+            >
+                {opt.name}
+            </span>
+
+            <div className="flex items-center mb-0">
+                <span
+                    className={`me-2 text-lg cursor-pointer ${checked ? "text-blue-600 font-medium" : "text-gray-900 font-light"
+                        }`}
                 >
                     +{formatRupiah(opt.price)}
-                </label>
+                </span>
+
                 <input
                     id={inputId}
                     type={type}
                     name={name}
                     checked={checked}
-                    onChange={onChange}
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
+                    readOnly
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 cursor-pointer"
                 />
-
             </div>
         </li>
     )
