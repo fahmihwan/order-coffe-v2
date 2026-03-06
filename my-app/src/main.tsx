513bec205c1c5849a3b42fa0cd552c156e38d87f
fahmihwan <50146950+fahmihwan@users.noreply.github.com>
2026-03-07 03:40:50 +0700

    remove error vscde

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,7 +1,6 @@
-import { StrictMode, Suspense } from 'react'
+import { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.tsx'
 import { RouterProvider } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './redux/store.ts';
